4c3b07a1e3020091d1fcf63f15f307576ed29cec
Jens Winkel <115979577+Arkcorner@users.noreply.github.com>
2024-10-20 14:54:22 +0200

    Modification Of Bow Script

---
+++ src/main/java/lunacy/fuelrods/de/item/custom/FuelStaffItem.java
@@ -0,0 +1,9 @@
+package lunacy.fuelrods.de.item.custom;
+
+import net.minecraft.item.Item;
+
+public class FuelStaffItem extends Item {
+    public FuelStaffItem(Settings settings) {
+        super(settings);
+    }
+}
